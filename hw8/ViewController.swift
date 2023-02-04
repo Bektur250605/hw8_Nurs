1904c6b51417503ce37da89d69f8f4c3f512ef8a
bektur <bektur060525@gmail.com>
2023-02-05 00:27:46 +0600

    Initial Commit

---
+++ hw8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw8
+//
+//  Created by Бектур Каримов on 5/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
